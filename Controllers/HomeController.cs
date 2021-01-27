addce1031f4a69380ef1bf16b2d7e3a841b171e1
Javed Ahmadzai <theahmadzai@hotmail.com>
2021-01-28 02:43:27 +0500

    Updated

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -3,31 +3,108 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using FoodInventory.Models;
 
 namespace FoodInventory.Controllers {
     public class HomeController : Controller {
+        POS_InventoryEntities db = new POS_InventoryEntities();
+
         public ActionResult Index() {
-            return View();
+            if(Session["U_Name"] != null) {
+                return View();
+            }
+
+            return RedirectToAction("Index", "Login");
         }
 
         public ActionResult MakeSales() {
-            return View();
+            if(Session["U_Name"] != null) {
+                return View();
+            }
+
+            return RedirectToAction("Index", "Login");
         }
 
+        [HttpGet]
         public ActionResult AddNewItem() {
+            if(Session["U_Name"] != null) {
+                return View();
+            }
+
+            return RedirectToAction("Index", "Login");
+        }
+
+        [HttpPost]
+        public ActionResult AddNewItem(Item item) {
+            try {
+                db.Items.Add(item);
+                db.SaveChanges();
+                ViewBag.Success = "Item has been added.";
+            } catch(Exception ex) {
+                ViewBag.Error = "Failed to save item.";
+            }
+
             return View();
         }
 
+        [HttpGet]
         public ActionResult AddStock() {
+            if(Session["U_Name"] != null) {
+                return View();
+            }
+
+            return RedirectToAction("Index", "Login");
+        }
+
+        [HttpPost]
+        public ActionResult AddStock(Item newItem) {
+            try {
+                Item item = db.Items.Find(newItem.Item_No);
+
+                if(item == null) {
+                    ViewBag.Error = "This item does not exist.";
+                } else {
+                    db.Items.Attach(item);
+
+                    item.Quantity = newItem.Quantity;
+
+                    db.Entry(item).Property(i => i.Quantity).IsModified = true;
+                    db.SaveChanges();
+                    ViewBag.Success = "Updated successfully.";
+                }
+            } catch(Exception ex) {
+                ViewBag.Error = "Failed to Update Stock.";
+            }
+
             return View();
         }
+
+        [HttpPost]
+        public ActionResult SearchItem(int? itemNo) {
+            Item item = db.Items.FirstOrDefault(i => i.Item_No == itemNo);
+
+            if(item != null) {
+                return View("AddStock", item);
+            }
+
+            ViewBag.SearchError = "Item with item no '" + itemNo + "' not found.";
+            return View("AddStock");
+        } 
 
         public ActionResult SalesReport() {
-            return View();
+            if(Session["U_Name"] != null) {
+                return View();
+            }
+
+            return RedirectToAction("Index", "Login");
         }
 
         public ActionResult StockReport() {
-            return View();
+            if(Session["U_Name"] != null) {
+                return View();
+            }
+
+            return RedirectToAction("Index", "Login");
         }       
     }
 }
--- Controllers/LoginController.cs
+++ Controllers/LoginController.cs
@@ -3,15 +3,44 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using FoodInventory.Models;
 
 namespace FoodInventory.Controllers
 {
     public class LoginController : Controller
     {
-        // GET: Login
+        POS_InventoryEntities db = new POS_InventoryEntities();
+
+        [HttpGet]
         public ActionResult Index()
         {
+            if(Session["U_Name"] != null) {
+                return RedirectToAction("Index", "Home");
+            }
+
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(string username, string password) {
+            var login = db.Logins.Where(u => u.U_Name.Equals(username) && u.Password.ToString() == password).FirstOrDefault();
+
+            if(login != null) {
+                Session["U_Name"] = login.U_Name.ToString();
+                Session["Password"] = login.Password.ToString();
+                Session["Role"] = login.Role.ToString();
+
+                return RedirectToAction("Index", "Home");
+
+            }
+
+            ViewBag.Error = "Invalid login credentials.";
+            return View("Index");
+        }
+
+        public ActionResult Logout() {
+            Session.Abandon();
+            return RedirectToAction("Index", "Login");
         }
     }
 }
